c429ea4505b7af8fa34f2f7bd0587ee06899414f
arshaksenin <arshaksenin@gmail.com>
2023-09-18 12:55:06 +0530

    initialise

---
+++ bin/first_sample_program.dart
@@ -0,0 +1,23 @@
+void main() 
+{
+  String name = 'aju';
+  int  age = 20;
+  int phone =9876543210;
+  double cgpa = 8.1;
+  String email ='ajumaon@gmail.com';
+
+  print('My name is $name ');
+  print('Iam $age years old');
+  print('my phone nuber is $phone');
+  print('my cgpa is $cgpa');
+  print('my emailid is $email');
+  
+
+
+
+
+
+  
+
+ 
+}
